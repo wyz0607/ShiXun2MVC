99575999db5c60040e74fe5a2c0ddb3e4da433e3
家铮 华 <1518316576@qq.com>
2019-02-15 16:04:14 +0800

    调整一下

--- Restaurant Information MVC/Controllers/ReceptionController.cs
+++ Restaurant Information MVC/Controllers/ReceptionController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Restaurant_Information_MVC;
+using Newtonsoft.Json;
 
 namespace Restaurant_Information_MVC.Controllers
 {
     public class ReceptionController : Controller
     {
         // GET: Reception
-        public ActionResult Index()
+        public ActionResult ShowOrder()
         {
+            string json=HttpClientHelper.Seng("get", "api/Reception/ShowOrder",null);
             return View();
         }
     }
