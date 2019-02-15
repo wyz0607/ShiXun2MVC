ecd441548ee0457fe07189df970ba75033ab6a53
张坤敏 <2606172373@qq.com>
2019-02-15 16:03:44 +0800

    厨房MVC的更改

--- Restaurant Information MVC/Controllers/KitchenController.cs
+++ Restaurant Information MVC/Controllers/KitchenController.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Restaurant_Information_MVC.Models;
+
+
 
 namespace Restaurant_Information_MVC.Controllers
 {
     public class KitchenController : Controller
     {
         // GET: Kitchen
-        public ActionResult Index()
+        
+        [HttpGet]
+        //显示
+        public ActionResult ShowMenu()
         {
             return View();
         }
+
+        [HttpGet]
+        //添加
+        public ActionResult AddMenu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMenu(KitchenViewModel kit)
+        {
+            return View();
+        }
+
+        //删除
+        public ActionResult DelMenu()
+        {
+            return View();
+        }
+
+        //修改
+        [HttpGet]
+        public ActionResult GetOneMenu(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UptMenu(KitchenViewModel kit)
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
     }
 }
