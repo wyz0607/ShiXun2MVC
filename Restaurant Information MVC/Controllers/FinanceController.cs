0c157e5e36b7ab5ed4e5c139616a3a041cb01662
YunWanJia <khz2630106690@163.com>
2019-02-18 16:53:36 +0800

    111

--- Restaurant Information MVC/Controllers/FinanceController.cs
+++ Restaurant Information MVC/Controllers/FinanceController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-
+using Restaurant_Information_MVC.Models;
+using Newtonsoft.Json;
 namespace Restaurant_Information_MVC.Controllers
 {
     public class FinanceController : Controller
     {
+        public static List<BillViewModel> bList;
         // GET: Finance
         /// <summary>
         /// 获取所有的账单信息
@@ -15,7 +19,34 @@ namespace Restaurant_Information_MVC.Controllers
         /// <returns></returns>
         public ActionResult ShowBill()
         {
-            return View();
+            bList = JsonConvert.DeserializeObject<List<BillViewModel>>(HttpClientHelper.Seng("get", "api/FinanceApi/ShowBill", null));
+            return View(bList);
+        }
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="begin">开始时间</param>
+        /// <param name="end">结束时间</param>
+        /// <param name="name">姓名</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult ShowBill(string begin,string end,string name)
+        {
+            if (begin != null && end != null)
+            {
+                bList = bList.Where(m => m.UserName.Contains(name) && Convert.ToDateTime(m.PaymentTime) >= Convert.ToDateTime(begin) && Convert.ToDateTime(m.PaymentTime) <= Convert.ToDateTime(end)).ToList();
+            }else if(begin ==null && end != null)
+            {
+                bList = bList.Where(m => m.UserName.Contains(name) && Convert.ToDateTime(m.PaymentTime) <= Convert.ToDateTime(end)).ToList();
+            }else if(begin !=null && end == null)
+            {
+                bList = bList.Where(m => m.UserName.Contains(name) && Convert.ToDateTime(m.PaymentTime) >= Convert.ToDateTime(begin)).ToList();
+            }
+            else
+            {
+                bList = bList.Where(m => m.UserName.Contains(name)).ToList();
+            }
+            return View(bList);
         }
         /// <summary>
         /// 审核账单
