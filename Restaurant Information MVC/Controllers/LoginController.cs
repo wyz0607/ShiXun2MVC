95e96ece386819eee267001c83a9bd2735f2faeb
wyz0607 <876468933@qq.com>
2019-02-18 13:47:04 +0800

    登录功能

--- Restaurant Information MVC/Controllers/LoginController.cs
+++ Restaurant Information MVC/Controllers/LoginController.cs
@@ -9,8 +9,16 @@ namespace Restaurant_Information_MVC.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Name, string Password)
+        {
+            var result= HttpClientHelper.Seng("get","api/Login/Login?UserName="+Name+"&Pwd="+Password+"",null);
             return View();
         }
     }
