fff760c412a9735cbf96d7e00d7cc94cb66c0967
张坤敏 <2606172373@qq.com>
2019-02-18 11:35:58 +0800

    厨房mvc和视图的创建

--- Restaurant Information MVC/Controllers/KitchenController.cs
+++ Restaurant Information MVC/Controllers/KitchenController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Restaurant_Information_MVC.Models;
+using Newtonsoft.Json;
 
 
 
@@ -12,6 +13,7 @@ namespace Restaurant_Information_MVC.Controllers
     public class KitchenController : Controller
     {
         // GET: Kitchen
+      
         
         [HttpGet]
         //显示
@@ -24,13 +26,31 @@ namespace Restaurant_Information_MVC.Controllers
         //添加
         public ActionResult AddMenu()
         {
+            //分类下拉列表
             return View();
         }
-
         [HttpPost]
-        public ActionResult AddMenu(KitchenViewModel kit)
+        public ActionResult AddMenu(KitchenViewModel kit,HttpPostedFileBase img)
         {
-            return View();
+            //判断是否有图片上传
+            if (img!=null)
+            {
+                //获取images绝对路径
+                string path = Server.MapPath("/Images/");
+                string fileName = DateTime.Now.ToString("yyyyMMddhhmmss")+img.FileName;
+                img.SaveAs(path+fileName);
+                kit.MenuPhoto = "http://localhost:53169/Images/" + fileName;
+            }
+            string strJson = JsonConvert.SerializeObject(kit);
+            string result = HttpClientHelper.Seng("post", "api/Kitchen/AddMenu",strJson);
+            if (result.Contains("成功"))
+            {
+                return Redirect("/Kitchen/ShowMenu");
+            }
+            else
+             {
+                return Content("<script>alert('"+ result +"')</script>");
+            }
         }
 
         //删除
