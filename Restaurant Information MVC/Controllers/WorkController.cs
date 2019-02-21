b8563cf72332d5acd27d50a41c41dc20a6d695ee
2225030605@qq.com <2225030605@qq.com>
2019-02-21 20:08:42 +0800

    邮箱验证

--- Restaurant Information MVC/Controllers/WorkController.cs
+++ Restaurant Information MVC/Controllers/WorkController.cs
@@ -8,11 +8,16 @@ using Restaurant_Information_MVC.Models;
 using Restaurant_Information_MVC.Controllers;
 using Newtonsoft.Json;
 using System.Text;
+using System.Net;
+using System.IO;
+using System.Net.Mail;
 
 namespace Restaurant_Information_MVC.Controllers
 {
     public class WorkController : Controller
     {
+
+
         // GET: Work
         /// <summary>
         /// 显示所有的评论
@@ -40,8 +45,8 @@ namespace Restaurant_Information_MVC.Controllers
             ViewBag.totalpage = (Math.Floor((list.Count() * 1.0) / 2)) + 1;
 
             return View(list.Skip((pageindex - 1) * 2).Take(2).ToList());
-            
-           
+
+
         }
         /// <summary>
         /// 登录信息
@@ -67,7 +72,7 @@ namespace Restaurant_Information_MVC.Controllers
         {
             var str = JsonConvert.SerializeObject(comment);
             string jsonstr = HttpClientHelper.Seng("put", "api/WorkApi/UptComment", str);
-            if(jsonstr.Contains("成功"))
+            if (jsonstr.Contains("成功"))
             {
                 return Content("操作成功");
             }
@@ -83,7 +88,7 @@ namespace Restaurant_Information_MVC.Controllers
         [HttpGet]
         public ActionResult Uptuser()
         {
-            int id =Convert.ToInt32(Response.Cookies["UserID"].Value);
+            int id = Convert.ToInt32(Response.Cookies["UserID"].Value);
             var str = HttpClientHelper.Seng("get", "api/WorkApi/GetOneUser/?userid=1", null);
             UserInfo user = JsonConvert.DeserializeObject<UserInfo>(str);
 
@@ -94,7 +99,7 @@ namespace Restaurant_Information_MVC.Controllers
         {
             var str = JsonConvert.SerializeObject(userInfo);
             string jsonstr = HttpClientHelper.Seng("put", "api/WorkApi/Uptuser", str);
-            if(jsonstr.Contains("成功"))
+            if (jsonstr.Contains("成功"))
             {
                 return Content("修改成功");
             }
@@ -102,7 +107,7 @@ namespace Restaurant_Information_MVC.Controllers
             {
                 return Content("修改失败");
             }
-   
+
         }
         /// <summary>
         /// 用户注册
@@ -116,7 +121,7 @@ namespace Restaurant_Information_MVC.Controllers
         /// 获取单个用户信息
         /// </summary>
         /// <returns></returns>
-      public ActionResult GetOneUser()
+        public ActionResult GetOneUser()
         {
             return View();
         }
@@ -186,12 +191,12 @@ namespace Restaurant_Information_MVC.Controllers
             int radom = Convert.ToInt32(result);
             CommonSmsRequest request = new CommonSmsRequest
             {
-               
-                Account = "17321446954",
-                Pwd = "e5a47ed56946ff3b78f32712d",//登录web平台 http://sms.feige.ee  在管理中心--基本资料--接口密码 或者首页 接口秘钥 如登录密码修改，接口密码会发生改变，请及时修改程序
+
+                Account = "17301622446",
+                Pwd = "58ed898d1d9ae5406147ee764",//登录web平台 http://sms.feige.ee  在管理中心--基本资料--接口密码 或者首页 接口秘钥 如登录密码修改，接口密码会发生改变，请及时修改程序
                 Content = $"亲，你的验证码是{radom}",
-                Mobile = "17321446954",
-                SignId = 95171, //登录web平台 http://sms.feige.ee  在签名管理中--新增签名--获取id
+                Mobile = "17301622446",
+                SignId = 95654, //登录web平台 http://sms.feige.ee  在签名管理中--新增签名--获取id
                 SendTime = Convert.ToInt64(common.ToUnixStamp(DateTime.Now))//定时短信 把时间转换成时间戳的格式
             };
             Session["Yan"] = radom;
@@ -229,15 +234,173 @@ namespace Restaurant_Information_MVC.Controllers
 
         }
 
+
+
+
+
+
+
+        //[HttpPost]
+        //public string Duanxin(string phone = "17321446954")
+        //{
+
+        //    //UTF-8格式：   String encode="UTF-8"
+        //    String encode = "UTF-8";  //页面编码和短信内容编码为GBK。重要说明：如提交短信后收到乱码，请将GBK改为UTF-8测试。如本程序页面为编码格式为：ASCII/GB2312/GBK则该处为GBK。如本页面编码为UTF-8或需要支持繁体，阿拉伯文等Unicode，请将此处写为：UTF
+        //    String username = "17321446954";  //用户名
+        //    String password_md5 = "snw123456";  ////32位MD5密码加密，不区分大小写
+        //    String mobile = $"{phone}";  //手机号,只发一个号码：13800000001。发多个号码：13800000001,13800000002,...N 。使用半角逗号分隔。
+        //    String apikey = "bfc73bab25ba0a15509494cfecdaf0d8";  //apikey秘钥（请登录 http://m.5c.com.cn 短信平台-->账号管理-->我的信息 中复制apikey）
+        //    string content = "您好！您的验证码是：1234【美联】";  //要发送的短信内容，特别注意：签名必须设置，网页验证码应用需要加添加【图形识别码】。;
+        //    String content2 = UrlEncode(content);
+        //    //新建一个StringBuilder类
+        //    StringBuilder sbTemp = new StringBuilder();
+
+
+        //    //发送链接（用户名，密码，手机号，apikey，短信内容，编码格式）
+        //    sbTemp.Append("username=" + username + "&password_md5=" + password_md5 + "&mobile=" + mobile + "&apikey=" + apikey + "&content=" + content2 + "&encode=" + encode);
+
+
+        //    //对短信内容做Urlencode编码操作。
+        //    byte[] bTemp = System.Text.Encoding.GetEncoding("UTF-8").GetBytes(sbTemp.ToString());
+        //    //发送返回的结果存入result中
+        //    String result = PostRequest("http://m.5c.com.cn/api/send/?", bTemp);  ////如连接超时，可能是您服务器不支持域名解析，请将下面连接中的：【m.5c.com.cn】修改为IP：【115.28.23.78】
+
+
+        //    //输出result内容，查看返回值，成功为success，错误为error，详见该文档起始注释
+        //    return result;
+        //}
+
+
+        ////POST方式发送得结果
+
+        //private static String PostRequest(string url, byte[] bData)
+        //{
+        //    System.Net.HttpWebRequest hwRequest;
+        //    System.Net.HttpWebResponse hwResponse;
+
+
+        //    string strResult = string.Empty;
+        //    try
+        //    {
+        //        //获取上面的URL链接
+        //        hwRequest = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+        //        //设置超时时间
+        //        hwRequest.Timeout = 5000;
+        //        //发送请求为POST
+        //        hwRequest.Method = "POST";
+        //        hwRequest.ContentType = "application/x-www-form-urlencoded";
+        //        //bData的长度（也就是获取用户名，密码，手机号，apikey，短信内容，编码格式总长度）
+        //        hwRequest.ContentLength = bData.Length;
+        //        //发送
+        //        System.IO.Stream smWrite = hwRequest.GetRequestStream();
+        //        smWrite.Write(bData, 0, bData.Length);
+        //        //释放资源
+        //        smWrite.Close();
+        //    }
+        //    catch (System.Exception err)
+        //    {
+        //        WriteErrLog(err.ToString());
+        //        return strResult;
+        //    }
+
+
+        //    //get response
+        //    try
+        //    {
+        //        //使用hwResponse来获取数据
+        //        hwResponse = (HttpWebResponse)hwRequest.GetResponse();
+        //        StreamReader srReader = new StreamReader(hwResponse.GetResponseStream(), Encoding.ASCII);
+        //        strResult = srReader.ReadToEnd();
+        //        srReader.Close();
+        //        hwResponse.Close();
+        //    }
+        //    catch (System.Exception err)
+        //    {
+        //        WriteErrLog(err.ToString());
+        //    }
+
+
+        //    return strResult;
+        //}
+        ////URLencode编码
+        //public static string UrlEncode(string str)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    byte[] byStr = System.Text.Encoding.UTF8.GetBytes(str); //默认是System.Text.Encoding.Default.GetBytes(str)
+        //    for (int i = 0; i < byStr.Length; i++)
+        //    {
+        //        sb.Append(@"%" + Convert.ToString(byStr[i], 16));
+        //    }
+
+
+        //    return (sb.ToString());
+        //}
+        //private static string WriteErrLog(string strErr)
+        //{
+
+        //    return strErr;
+        //}
+        public  string Yanzheng;
+        [HttpGet]
+        public string SendEmail(string email)
+        {
+            Random rd = new Random();
+            string validateCode = rd.Next(100000, 1000000).ToString();
+            Email e = new Email()
+            {
+                host = "smtp.qq.com",
+                mailBody = "验证码：" + validateCode,
+                mailFrom = "876468933@qq.com",
+                mailPwd = "pqywqeahvesvbfgf",
+                mailSubject = "[一份来自测试的邮件]",
+                mailToArray = new string[] { email }
+            };
+            e.Send();
+            //Session["Yanzhengma"] = validateCode;
+            //ViewBag.yanzheng = validateCode;
+            //Yanzheng = validateCode;
+            //if(validateCode!=null)
+            //{
+            //    return  validateCode;
+            //}
+            //else
+            //{
+            //    return "发送失败";
+            //}
+            return validateCode;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
     public enum Stateinfo
     {
-        待审核=0,
-        审核通过=1,
-        驳回=2
+        待审核 = 0,
+        审核通过 = 1,
+        驳回 = 2
 
     }
- 
+
     public class CommonSmsRequest
     {
         public string Account { get; set; }
@@ -276,4 +439,153 @@ namespace Restaurant_Information_MVC.Controllers
         /// </summary>
         public int BlackCount { get; set; }
     }
+
+
+
+
+
+
+
+
+
+
+
+    public class Email
+    {
+        /// <summary>
+        /// 发送者
+        /// </summary>
+        public string mailFrom { get; set; }
+
+        /// <summary>
+        /// 收件人
+        /// </summary>
+        public string[] mailToArray { get; set; }
+
+        /// <summary>
+        /// 抄送
+        /// </summary>
+        public string[] mailCcArray { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string mailSubject { get; set; }
+
+        /// <summary>
+        /// 正文
+        /// </summary>
+        public string mailBody { get; set; }
+
+        /// <summary>
+        /// 发件人密码
+        /// </summary>
+        public string mailPwd { get; set; }
+
+        /// <summary>
+        /// SMTP邮件服务器
+        /// </summary>
+        public string host { get; set; }
+
+        /// <summary>
+        /// 正文是否是html格式
+        /// </summary>
+        public bool isbodyHtml { get; set; }
+
+        /// <summary>
+        /// 附件
+        /// </summary>
+        public string[] attachmentsPath { get; set; }
+
+        public bool Send()
+        {
+            //使用指定的邮件地址初始化MailAddress实例
+            MailAddress maddr = new MailAddress(mailFrom);
+            //初始化MailMessage实例
+            MailMessage myMail = new MailMessage();
+
+
+            //向收件人地址集合添加邮件地址
+            if (mailToArray != null)
+            {
+                for (int i = 0; i < mailToArray.Length; i++)
+                {
+                    myMail.To.Add(mailToArray[i].ToString());
+                }
+            }
+
+            //向抄送收件人地址集合添加邮件地址
+            if (mailCcArray != null)
+            {
+                for (int i = 0; i < mailCcArray.Length; i++)
+                {
+                    myMail.CC.Add(mailCcArray[i].ToString());
+                }
+            }
+            //发件人地址
+            myMail.From = maddr;
+
+            //电子邮件的标题
+            myMail.Subject = mailSubject;
+
+            //电子邮件的主题内容使用的编码
+            myMail.SubjectEncoding = Encoding.UTF8;
+
+            //电子邮件正文
+            myMail.Body = mailBody;
+
+            //电子邮件正文的编码
+            myMail.BodyEncoding = Encoding.Default;
+
+            myMail.Priority = MailPriority.High;
+
+            myMail.IsBodyHtml = isbodyHtml;
+
+            //在有附件的情况下添加附件
+            try
+            {
+                if (attachmentsPath != null && attachmentsPath.Length > 0)
+                {
+                    Attachment attachFile = null;
+                    foreach (string path in attachmentsPath)
+                    {
+                        attachFile = new Attachment(path);
+                        myMail.Attachments.Add(attachFile);
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                throw new Exception("在添加附件时有错误:" + err);
+            }
+
+            SmtpClient smtp = new SmtpClient();
+            //指定发件人的邮件地址和密码以验证发件人身份
+            smtp.Credentials = new System.Net.NetworkCredential(mailFrom, mailPwd);
+
+
+            //设置SMTP邮件服务器
+            smtp.Host = host;
+
+            try
+            {
+                //将邮件发送到SMTP邮件服务器
+                smtp.Send(myMail);
+                return true;
+
+            }
+            catch (System.Net.Mail.SmtpException)
+            {
+                return false;
+            }
+
+        }
+    }
+
+
+
+
+
+
+
 }
