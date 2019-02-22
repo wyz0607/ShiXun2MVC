37fda9414518a3774e4cd18d5dc8a9415fea1184
wyz0607 <876468933@qq.com>
2019-02-22 20:05:04 +0800

    登录页面优化

--- Restaurant Information MVC/Controllers/LoginController.cs
+++ Restaurant Information MVC/Controllers/LoginController.cs
@@ -41,7 +41,8 @@ namespace Restaurant_Information_MVC.Controllers
         public ActionResult Show()
         {
             var time = DateTime.Now.ToString("yyyy年MM月dd日");
-
+           
+            
             var num= HttpClientHelper.Seng("get", "api/Login/GetOrderNum?time=" + time,null);
             ViewBag.OrderNum = num;
             var oneMoney = HttpClientHelper.Seng("get", "api/Login/GetOneMoney?time=" + time, null);
@@ -50,7 +51,62 @@ namespace Restaurant_Information_MVC.Controllers
             ViewBag.menu = menu;
             var money = HttpClientHelper.Seng("get", "api/Login/GetAllMoney", null);
             ViewBag.money = money;
+            
+
             return View();
+        }
+
+        public ActionResult GetResult()
+        {
+            List<int> getDay = new List<int>();
+            var yea = DateTime.Now.Year.ToString();
+            var mon = DateTime.Now.Month.ToString();
+            var day = DateTime.Now.Day;
+            var tiao = day - 7;
+
+            for (int i = day; tiao< i; i--)
+            {
+                string shi = "";
+                if (Convert.ToInt32( mon)<10 && day<10)
+                {
+                     shi = yea + "年" + "0" + mon + "月" + "0" + day + "日";
+                    
+                }
+                else if (Convert.ToInt32(mon)<10)
+                {
+                    shi = yea + "年" + "0" + mon + "月" + day + "日";
+                }
+                else if (day<10)
+                {
+                    shi = yea + "年"+ mon + "月" + "0" + day + "日";
+                }
+                else
+                {
+                    shi = yea + "年"+ mon + "月" + day + "日";
+                }
+
+                var numLine = HttpClientHelper.Seng("get", "api/Login/GetOrderNum?time=" + shi, null);
+                getDay.Add(Convert.ToInt32(numLine));
+                day = day - 1;
+
+            }
+            return Content(JsonConvert.SerializeObject(getDay));
+        }
+
+        public ActionResult GetArr()
+        {
+            List<int> getDay = new List<int>();
+            var yea = DateTime.Now.Year.ToString();
+            var mon = DateTime.Now.Month.ToString();
+            var day = DateTime.Now.Day;
+            var tiao = day - 7;
+
+            for (int i = day; tiao < i; i--)
+            {
+                getDay.Add(day);
+                day = day - 1;
+            }
+            return Content(JsonConvert.SerializeObject(getDay));
         }
     }
 }
