96abdc6fe70153d2a09189a2fa67b04a62573480
2225030605@qq.com <2225030605@qq.com>
2019-03-18 15:25:04 +0800

    1

--- Restaurant Information MVC/Controllers/WorkController.cs
+++ Restaurant Information MVC/Controllers/WorkController.cs
@@ -28,7 +28,7 @@ namespace Restaurant_Information_MVC.Controllers
         public ActionResult ShowComment(int pageindex = 1, int pagesize = 5)
         {
             var str = HttpClientHelper.Seng("get", "api/WorkApi/ShowComment", null);
-            var str1 = HttpClientHelper.Seng("get","",null);
+            var str1 = HttpClientHelper.Seng("get", "api/ReceptionApi/ShowOrder", null);
             List<CommentViewModel> list1 = JsonConvert.DeserializeObject<List<CommentViewModel>>(str);
             List<OrderViewModel> list2 = JsonConvert.DeserializeObject<List<OrderViewModel>>(str1);
             var list = from s in list1.AsEnumerable()
@@ -159,9 +159,17 @@ namespace Restaurant_Information_MVC.Controllers
         /// 删除用户信息
         /// </summary>
         /// <returns></returns>
-        public ActionResult DelUser()
+        public ActionResult DelUser(int id)
         {
-            return View();
+            string jsonstr = HttpClientHelper.Seng("put", "api/WorkApi/DelUserinfo?id="+id,null );
+            if(jsonstr.Contains("成功"))
+            {
+                return Redirect("/Work/ShowUserinfo");
+            }
+            else
+            {
+                return Content("删除失败");
+            }
         }
         
         /// <summary>
@@ -173,9 +181,15 @@ namespace Restaurant_Information_MVC.Controllers
             return View();
         }
         /// <summary>
-        /// 添加员工
+        /// 添加角色
         /// </summary>
         /// <returns></returns>
+        [HttpGet]
+        public ActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
        public ActionResult AddRole(RoleViewModel roleView)
         {
             var dd = JsonConvert.SerializeObject(roleView);
@@ -190,6 +204,31 @@ namespace Restaurant_Information_MVC.Controllers
             }
         }
         /// <summary>
+        /// 添加员工
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddUserInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddUserinfo(UserInfo userInfo)
+        {
+            var dd = JsonConvert.SerializeObject(userInfo);
+            var str = HttpClientHelper.Seng("post","api/WorkApi/AddUser",dd);
+            if(str.Contains("成功"))
+            {
+                return Content("添加成功");
+            }
+            else
+            {
+                return Content("添加失败");
+            }
+
+
+
+        }
+        /// <summary>
         /// 显示所有的角色信息
         /// </summary>
         /// <returns></returns>
@@ -202,13 +241,30 @@ namespace Restaurant_Information_MVC.Controllers
 
         }
         /// <summary>
-        /// 显示所有员工的信息
+        /// 显示所有角色的信息
         /// </summary>
         /// <returns></returns>
         public ActionResult ShowEmp()
         {
             var str = HttpClientHelper.Seng("get","api/WorkApi/ShowUserinfo",null);
             List<UserInfo> list = JsonConvert.DeserializeObject<List<UserInfo>>(str);
+            return View(list);
+        }
+        /// <summary>
+        /// 显示所有员工的信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ShowUserInfo()
+        {
+            var str = HttpClientHelper.Seng("get", "api/WorkApi/ShowUserinfo", null);
+            List<UserInfo> list = JsonConvert.DeserializeObject<List<UserInfo>>(str).ToList();
+            var list1 = from s in list
+                        select new UserInfo
+                        {
+                            UserName = s.UserName,
+                            UserSex = s.UserSex,
+                            Rname = s.Rname
+                        };
             return View(list);
         }
         /// <summary>
