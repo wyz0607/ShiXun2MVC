d2863ec9d4d15ba37a384d8bbb92ffe211a7e3c4
张坤敏 <2606172373@qq.com>
2019-03-18 20:19:48 +0800

    123

--- Restaurant Information MVC/Controllers/WorkController.cs
+++ Restaurant Information MVC/Controllers/WorkController.cs
@@ -308,11 +308,25 @@ namespace Restaurant_Information_MVC.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
+        
+        [HttpGet]
         public ActionResult ShowProposer()
         {
             var str = HttpClientHelper.Seng("get", "api/WorkApi/GetProposers", null);
-            ProposerViewModel proposerView = JsonConvert.DeserializeObject<List<ProposerViewModel>>(str).FirstOrDefault();
-            return View(proposerView);
+            List<ProposerViewModel> proposerView = JsonConvert.DeserializeObject<List<ProposerViewModel>>(str);
+            var list = from s in proposerView.AsEnumerable()
+                       select new ProperOrderSherd
+                       {
+                           ProposerId = s.ProposerId,
+                           ShareHolderId = s.ShareHolderId,
+                           ProposerCause = s.ProposerCause,
+                           ProposerState = s.ProposerState,
+                           ProposerTime = s.ProposerTime,
+                           EndTime = s.EndTime,
+                           Uname = s.Menu.HolderName,
+                           Rname = s.Menu.Roles.RoleName
+                       };
+            return View(list);
         }
 
         public  string Yanzheng;
