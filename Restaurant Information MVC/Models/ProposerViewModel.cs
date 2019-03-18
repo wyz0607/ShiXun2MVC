9bf3f24e2574e0856afa18b84b9fc522f6576968
家铮 华 <1518316576@qq.com>
2019-03-18 18:55:10 +0800

    1

--- Restaurant Information MVC/Models/ProposerViewModel.cs
+++ Restaurant Information MVC/Models/ProposerViewModel.cs
@@ -8,13 +8,13 @@ namespace Restaurant_Information_MVC.Models
     public class ProposerViewModel
     {
         public int ProposerId { get; set; }
-        public int RoleId { get; set; }
+        public int ShareHolderId { get; set; }
         public string ProposerCause { get; set; }
         public string ProposerTime { get; set; }
         public string StateTime { get; set; }
         public string EndTime { get; set; }
         public int ProposerState { get; set; }
 
-        public virtual RoleViewModel Menu { get; set; }
+        public virtual ShareViewModel Menu { get; set; }
     }
 }
